5cf156b755d3b30d53cf7bbe347d4d135555faa5
MS2104 <31002@ma-web.nl>
2022-04-14 11:46:16 +0200

    Missing scripts added. For major changes please view the README.md

--- Assets/Scripts/Combat Scripts/Target.cs
+++ Assets/Scripts/Combat Scripts/Target.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,13 @@ using UnityEngine;
 public class Target : MonoBehaviour, IDamageable
 {
     [SerializeField] private float health = 100f;
+    [SerializeField] ParticleSystem collectParticle = null;
+
+    public void Update()
+    {
+        
+    }
+
     public void TakeDamage(float damage)
     {
         health -= damage;
@@ -12,6 +20,12 @@ public class Target : MonoBehaviour, IDamageable
         {
             Debug.Log("Enemy killed!");
             Destroy(gameObject);
+            Collect();
         }
+    }
+
+    public void Collect()
+    {
+        collectParticle.Play();
     }
 }
